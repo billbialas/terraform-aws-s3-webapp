31476e4568958370f076850b2a2e2fdb31bc013e
Bill Bialas <bill.bialas@gmail.com>
2024-04-20 20:27:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,15 +7,6 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_ownership_controls" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   rule {
